11752c916f879ab8347d518b536d3b123dfa5d67
Andres Muñoz <andru2015.7@gmail.com>
2024-04-19 19:31:13 -0500

    first commit

---
+++ LinqQueries.cs
@@ -0,0 +1,17 @@
+public class LinqQueries 
+{
+    private List<Book> booksCollection = new List<Book>();
+    public LinqQueries()
+    {
+        using(StreamReader reader = new StreamReader("books.json")) 
+        {
+            string json = reader.ReadToEnd();
+            booksCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true })!;
+        }
+    }
+
+    public IEnumerable<Book> WholeCollection() 
+    {
+        return booksCollection;
+    }
+}
