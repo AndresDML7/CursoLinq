6c3a4cf6e0cb6749274386c079d7f120b15351d6
Andres Muñoz <andru2015.7@gmail.com>
2024-04-19 21:30:15 -0500

    Adding some queries to print data

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 public class LinqQueries 
 {
     private List<Book> booksCollection = new List<Book>();
@@ -13,5 +15,23 @@ public class LinqQueries
     public IEnumerable<Book> WholeCollection() 
     {
         return booksCollection;
+    }
+
+    public IEnumerable<Book> BooksAfter2000() 
+    {
+        //Extension method
+        return booksCollection.Where(p => p.PublishedDate.Year > 2000);
+
+        //Query expresion
+        //return from l in booksCollection where l.PublishedDate.Year > 2000 select l;
+    }
+
+    public IEnumerable<Book> BooksSecondCondition() 
+    {
+        //Extension method
+        //return booksCollection.Where(p => p.PageCount > 250 && p.Title.Contains("in Action"));
+
+        //Query expresion
+        return from l in booksCollection where l.PageCount > 250 && l.Title.Contains("in Action") select l;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+
+//PrintValues(queries.WholeCollection());
+
+//PrintValues(queries.BooksAfter2000());
+
+PrintValues(queries.BooksSecondCondition());
+
+void PrintValues(IEnumerable<Book> bookList) 
+{
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+
+    foreach(var item in bookList)
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
